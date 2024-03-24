37b97f95dc22231ffc3379f4442c031e32be637b
fenghe666666 <151989859+fenghe666666@users.noreply.github.com>
2024-03-24 12:09:27 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,9 +4,9 @@ const CONFIG = {
   HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+  HEO_NOTICE_BAR: [ 
+    { title: '欢迎来到新趣帮', url: '#' }, 
+    { title: '发现有趣新应用！', url: '#' } 
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -20,7 +20,7 @@ const CONFIG = {
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_LINK: '#', 
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
